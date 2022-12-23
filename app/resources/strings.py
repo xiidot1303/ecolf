01585115abc43e9f1c0c48d8b598769b70de54c6
xiidot1303 <xii1303@inbox.ru>
2022-12-23 18:44:18 +0500

    update text

--- app/resources/strings.py
+++ app/resources/strings.py
@@ -1,10 +1,217 @@
 lang_dict = {
 
-    "": ["", "", ""],
+    "home": ["Asosiy", "Главная", "Home"],
 
-    "": ["", "", ""],
+    "about": ["Biz haqimizda", "О нас", "About"],
 
-    "": ["", "", ""],
+    "about us": ["Biz haqimizda", "О нас", "About us"],
+
+    "service": ["Servis", "Сервис", "Service"],
+
+    "product": ["Mahsulot", "Продукты", "Product"],
+
+    "contact": ["Aloqa", "Контакт", "Contact"],
+
+    "contact us": ["Biz bilan bog'laning", "Связаться с нами", "Contact us"],
+
+    "organic vegetables": ["Organik mevalar", "Органические фрукты", "Organic vegetables"],
+
+    "organic vegetables for healthy life": [
+        "Sog'lom hayot uchun organik mevalar", 
+        "Органические фрукты для здорового образа жизни", 
+        "Organic vegetables for healthy life"
+        ],
+
+    "explore": ["", "", ""],
+
+    "dry fruits": ["", "", ""],
+
+    "dry fruits text": ["", "", ""],
+
+    "organic fruits": ["", "", ""],
+
+    "organic fruits text": ["", "", ""],
+
+    "we produce organic food for your family": [
+        "Biz sizning oilangiz uchun organik mahsulotlar ishlab chiqaramiz", 
+        "Мы производим органические продукты питания для вашей семьи", 
+        "We produce organic food for your family"
+        ],
+
+    "we produce organic food for your family text": [
+        "Eco Life Fruits - quritilgan mevalar va yong'oqlar ishlab chiqaruvchisi va eksportchisi. Ayni paytda “Eco Life Fruits” O‘zbekistonda ushbu sohadagi yetakchi kompaniyalardan biri hisoblanadi. Nisbatan qisqa vaqt ichida kompaniya jahon bozoriga chiqish uchun barcha pozitsiyalarda jadal rivojlandi", 
+        "Компания Eco Life Fruits является производителем и экспортером сухофруктов и орехов. В настоящее время Eco Life Fruits, является одной из ведущих компаний в этом секторе в Узбекистане. За относительно небольшой период времени  компания динамично развивалась по всем позициям, чтобы войти на мировой рынок"
+        "Eco Life Fruits is a manufacturer and exporter of dried fruits and nuts. Currently, Eco Life Fruits is one of the leading companies in this sector in Uzbekistan. In a relatively short period of time, the company has dynamically developed in all positions in order to enter the global market", 
+    ],
+
+    "organic": ["Organik", "Органик", "Organic"],
+
+    "award winning": ["Mukofot sovrindori", "Удостоенный наград", "Award winning"],
+
+    "our experience": ["Bizning tajribamiz", "Наш опыт", "Our experience"],
+
+    "farm specialist": ["Mutaxasislar", "Специалисты", "Specialists"],
+
+    "complete project": ["Hamkorliklar", "Завершенные проекты", "Complete project"],
+
+    "happy clients": ["Baxtli mijozlar", "Счастливые клиенты", "Happy clients"],
+
+    "services": ["Servislar", "Сервисы", "Services"],
+
+    "organic farm services": ["", "", ""],
+
+    "organic farm services text": [
+        "", 
+        "", 
+        "In the production process, advanced technologies are used, which guarantee high quality of the goods, without the addition of preservatives, flavorings and chemical additives"
+        ],
+
+    "fresh vegetables": ["", "", ""],
+
+    "fresh vegetables text": ["", "", ""],
+
+    "fresh fruits": ["", "", ""],
+
+    "fresh fruits text": ["", "", ""],
+
+    "healty cattle": ["", "", ""],
+
+    "healty cattle text": ["", "", ""],
+
+    "modern truck": ["", "", ""],
+
+    "modern truck text": ["", "", ""],
+
+    "farming plans": ["", "", ""],
+
+    "farming plans text": ["", "", ""],
+
+    "features": ["Xususiyatlar", "Особенности", "Features"],
+
+    "features text": [
+        "Eco Life Fruits bodom, quritilgan o'rik, mayiz, uzum barglari va moylarini ulgurji sotish uchun barcha zarur Evropa va jahon sifat standartlari bo'yicha sertifikatlangan.", 
+        "Eco Life Fruits сертифицирована по всем необходимым европейским и мировым стандартам качества для оптовых продаж миндаля, кураги, изюма, виноградных листьев и масел экспортируемые оптом по всему миру из Узбекистана.", 
+        "Eco Life Fruits is certified according to all the necessary European and international quality standards for wholesale sales of almonds, dried apricots, raisins, grape leaves and oils exported in bulk worldwide from Uzbekistan."
+        ],
+
+    "why choose us!!!": ["Nega bizni tanlashingiz kerak!!!", "Почему вы должны выбрать нас!!!", "Why choose us!!!"],
+
+    "organic text": [
+        "Konservantlar, shakar va kimyoviy qo'shimchalar qo'shilmaydi", 
+        "Без добавлений консервантов, ароматизаторов и  химических добавок", 
+        "Without the addition of preservatives, flavorings and chemical additives"
+        ],
+
+    "modern farming": ["Zamonaviy texnologiyalar", "", "Modern farming"],
+
+    "modern farming text": [
+        "Ishlab chiqarish jarayonida ilg'or texnologiyalar qo'llaniladi, bu esa tovarlarning yuqori sifatini kafolatlaydi, ular yuklarning yuqori sifatini kafolatlaydi.", 
+        "В производственном процессе используется передовые технологии, что гарантируют высокое качество товара, без добавлений консервантов, ароматизаторов и  химических добавок", 
+        "In the production process, advanced technologies are used, which guarantee high quality of the goods."
+        ],
+
+    "support": ["Yordam", "Суппорт", "Support"],
+
+    "best price": ["Qulay narx", "Лучшая цена", "Best price"],
+
+    "best price text": [
+        "Biz birinchi qo'ldan ishlaganimiz uchun, narxlar arzonroq", 
+        "Мы работаем из первых рук, поэтому наши цены намного ниже, чем у некоторых конкурентов.", 
+        "We work first-hand, so our prices are much lower than some competitors."
+    ],
+
+    "award winning text": [
+        "Taniqli HACCP va ISO 9001 sertifikatlari bilan oziq-ovqat xavfsizligi sohasida", 
+        "С признанными сертификатами HACCP  и ISO 9001 в области безопасности пищевых продуктов", 
+        "With recognized HACCP and ISO 9001 certificates in the field of food safety"
+        ],
+
+    "products": ["Mahsulotlar", "", "Products"],
+
+    "our fresh & organic products": ["Sof & organik mahsulotlar", "Наши свежие и органические продукты", "Our fresh & organic products"],
+
+    "feedback1": [
+        """
+		We have been cooperating with Eco Life Fruits for many years. Since these
+		periods of cooperation, there has never been any coldness between us, and we are
+		ready to cooperate in the futurentore Sed ut perspiciatis unde omnis iste natus
+		error sit voluptatem accusantium
+        """,					 
+    
+        """
+		We have been cooperating with Eco Life Fruits for many years. Since these
+		periods of cooperation, there has never been any coldness between us, and we are
+		ready to cooperate in the futurentore Sed ut perspiciatis unde omnis iste natus
+		error sit voluptatem accusantium
+        """,					 
+    
+        """
+		We have been cooperating with Eco Life Fruits for many years. Since these
+		periods of cooperation, there has never been any coldness between us, and we are
+		ready to cooperate in the futurentore Sed ut perspiciatis unde omnis iste natus
+		error sit voluptatem accusantium
+        """,					 
+    
+        ],
+
+    "feedback2": [
+        """
+		è da tempo che cerchiamo un partner adatto per la nostra azienda. Ne è valsa la
+		pena la "Eco Life Fruits" LLC. perché ci forniscono i migliori prodotti sin dal
+		primo giorno della nostra collaborazione e ne siamo molto felici.					
+        """, 
+        """
+		è da tempo che cerchiamo un partner adatto per la nostra azienda. Ne è valsa la
+		pena la "Eco Life Fruits" LLC. perché ci forniscono i migliori prodotti sin dal
+		primo giorno della nostra collaborazione e ne siamo molto felici.					
+        """, 
+        """
+		è da tempo che cerchiamo un partner adatto per la nostra azienda. Ne è valsa la
+		pena la "Eco Life Fruits" LLC. perché ci forniscono i migliori prodotti sin dal
+		primo giorno della nostra collaborazione e ne siamo molto felici.					
+        """, 
+        
+        ],
+
+    "client name1": ["David Morgan", "David Morgan", "David Morgan"],
+
+    "client name2": ["Giovanni Roberto", "Giovanni Roberto", "Giovanni Roberto"],
+
+    "get in touch": ["Bog'lanish", "Связаться", "Get in touch"],
+
+    "quick links": ["Tezkor havolalar", "Быстрые ссылки", "Quick links"],
+
+    "newsletter": ["Xabarnoma", "Рассылка", "Newsletter"],
+
+    "subscribe our newsletter": ["Bizning xabarnomaga obuna", "Подпишитесь на нашу рассылку новостей", "Subscribe our newsletter"],
+
+    "your email": ["Email", "Email", "Your email"],
+
+    "sign up": ["OK", "Подписывать", "Sign up"],
+
+    "our services": ["Bizning servislarimiz", "Наши услуги", "Our services"],
+
+    "our products": ["Bizning mahsulotlarimiz", "Наши продукты", "Our producst"],
+
+    "please feel free to contact us": [
+        "Iltimos, biz bilan bog'laning", 
+        "Пожалуйста, не стесняйтесь обращаться к нам", 
+        "Please feel free to contact us"
+        ],
+
+    "your name": ["Ismingiz", "Ваше имя", "Your name"],
+
+    "subject": ["Mavzu", "Тема", "Subject"],
+
+    "message": ["Xabar", "Сообщение", "Message"],
+
+    "send message": ["Jo'natish", "Отправить", "Send message"],
+
+    "our office": ["Bizning offis", "Наш офис", "Our office"],
+
+    "email": ["Email", "Email", "Email"],
+
+    "call us": ["Telefon", "Телефон", "Call us"],
 
     "": ["", "", ""],
 
